ba75743acb9fe1dc75616e5996b068274daa9b59
AgentBoo <zuzi.han@gmail.com>
2017-11-05 19:00:38 -0500

    restructured file tree and added files in compliance with structure generated using express-generator

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-// NOTE: Modules =================================================================
+// NOTE: Module dependencies =====================================================
 const express = require('express'),
       bodyParser = require('body-parser'),
+      cookieParser = require('cookie-parser'),
       morgan = require('morgan'),
-      mustacheExpress = require('mustache-express');
+      mustacheExpress = require('mustache-express'),
+      path = require('path');
+// Routers
+const todos = require('./routes/todos'),
+      users = require('./routes/users');
 
 // NOTE: Mongo config ============================================================
 
@@ -10,15 +15,36 @@ const express = require('express'),
 const app = express();
       app.engine('mustache', mustacheExpress());
       app.set('view engine', 'mustache');
-      app.set('views', __dirname + '/views');
-      app.use('/public', express.static(__dirname + '/public'));
+      app.set('views', path.join(__dirname + 'views'));
+      app.use('/public', express.static(path.join(__dirname, 'public')));
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(cookieParser())
       app.use(morgan('dev'));
+      // Routers
+      app.use('/', todos);
+      app.use('/users', users);
+      // catch 404 and forward to error handler
+      app.use(function(req, res, next) {
+          let err = new Error('Not Found');
+          err.status = 404;
+          next(err);
+      });
+      // error handler
+      app.use(function(err, req, res, next) {
+          // set locals, only providing error in development
+          res.locals.message = err.message;
+          res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+          // render the error page
+          res.status(err.status || 500);
+          res.render('error');
+      });
+
 
 // NOTE: Index page
-app.get('/', (req, res) => res.send('You landed on the index page'));
+      app.get('/', (req, res) => res.send('You landed on the index page'));
 
-// NOTE: Port ====================================================================
-      app.set('port', (process.env.PORT || 5000));
-      app.listen(app.get('port'), () => console.log('Iscreamyouscreamgimmethatgimmethat ice cream, on port ', app.get('port')));
+
+// NOTE: Export to bin/www ========================================================
+module.exports = app;
